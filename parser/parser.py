24d86e370219cc5e939a5c54663e148587252e9f
Franck WANG <franckwang123@gmail.com>
2016-11-02 00:08:51 +0100

    remove columns filtering

--- parser/parser.py
+++ parser/parser.py
@@ -32,28 +32,13 @@ def txt_parser(file_name):
     """
 Read file_name and parse the data
 """
-    with open('./signals_to_monitor.txt') as f_signals_to_monitor:
-        signals_to_monitor = f_signals_to_monitor.readlines()
-        # Some names contains
-        get_name = lambda x: x.split("@")[1] if "@" in x else x
-        signals_to_monitor = [get_name(s).strip() for s in signals_to_monitor]
     # Fist 7 lines are description of the flight
     # Line 9 is the name of each column in french but it's not usable because
     # some have spaces like "FLIGHT DECK AMBIENT TEMPERATURE 1"
     # Line 10 contains the units of each line
     # Line 11 gives aquisition time TO_CHECK!!!!
-    df = pd.read_csv(file_name, header=7, skiprows=[9, 10, 11], delim_whitespace=True)
-    # Filter the columns to get only the signals we need
-    not_in_table = []
-    to_keep = []
-    for sig in signals_to_monitor:
-        if sig in df.columns:
-            to_keep.append(sig)
-        else:
-            logging.warning(sig+' was not found in the data')
-            not_in_table.append(sig)
-    logging.warning(str(len(not_in_table))+" not found")
-    return df[to_keep]
+    return pd.read_csv(file_name, header=7, skiprows=[9, 10, 11], delim_whitespace=True)
+
 
 if __name__ == "__main__":
     import argparse
