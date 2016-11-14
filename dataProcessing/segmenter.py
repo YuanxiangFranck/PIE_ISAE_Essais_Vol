462aa2d453bd0b24decf6ad5cc43b965e2992832
Quentin <qjacob.jacob@gmail.com>
2016-11-14 07:56:57 +0100

    change output types in segmenter.py

--- dataProcessing/segmenter.py
+++ dataProcessing/segmenter.py
@@ -31,66 +31,65 @@ altitude = 'ADSP1 Pressure Altitude (feet)'
 altitude_rate = 'ADSP1 Altitude Rate (ft/min)'
 calib_air_speed = 'ADSP1 Calibrated Airspeed (knots)'
 
-
 def get_on_the_ground(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est au sol
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est au sol
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
-    return data.loc[(wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)]
+    return data.loc[(wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)].Time
 
 def get_take_off(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est en phase de decollage
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est en phase de decollage
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
-    return data.loc[(~on_the_ground) & (altitude_signal < 6000)]
+    return data.loc[(~on_the_ground) & (altitude_signal < 6000)].Time
 
 def get_climb(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est en phase de montée
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est en phase de montée
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
     alt_rate_signal = data[altitude_rate].iloc[:,0]
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
-    return data.loc[(~on_the_ground) & (altitude_signal >6000) & (alt_rate_signal > 500)]
+    return data.loc[(~on_the_ground) & (altitude_signal >6000) & (alt_rate_signal > 500)].Time
 
 def get_hold(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est en phase d'attente
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est en phase d'attente
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
     alt_rate_signal = data[altitude_rate].iloc[:,0]
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
-    return data.loc[(~on_the_ground) & (altitude_signal > 6000) &(altitude_signal < 25000) & (alt_rate_signal > -500) & (alt_rate_signal < 500)]
+    return data.loc[(~on_the_ground) & (altitude_signal > 6000) &(altitude_signal < 25000) & (alt_rate_signal > -500) & (alt_rate_signal < 500)].Time
 
 def get_cruise(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est en phase de croisiere
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est en phase de croisiere
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
     alt_rate_signal = data[altitude_rate].iloc[:,0]
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
-    return data.loc[(~on_the_ground) & (altitude_signal > 25000) & (alt_rate_signal > -500) & (alt_rate_signal < 500)]
+    return data.loc[(~on_the_ground) & (altitude_signal > 25000) & (alt_rate_signal > -500) & (alt_rate_signal < 500)].Time
 
 def get_descent(data):
     """
-    Renvoie le dataframe extrait de data, contenant uniquement les signaux lorsque l'avion est en phase de descente
+    Renvoie la série extraite de data.Time, contenant uniquement les instants lorsque l'avion est en phase de descente
     """
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
     alt_rate_signal = data[altitude_rate].iloc[:,0]
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
-    return data.loc[(~on_the_ground) & (alt_rate_signal < -500)]
+    return data.loc[(~on_the_ground) & (alt_rate_signal < -500)].Time
