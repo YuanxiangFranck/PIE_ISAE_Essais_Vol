69be57124da7ea14a01e99b74468a1dcfa12f6fb
PRIEM <priem.remy.perso@gmail.com>
2016-11-20 17:45:36 +0100

    Correction of NNeighbors + deletion of bigger and klower (useless)

--- algorithms/NNeighbor.py
+++ algorithms/NNeighbor.py
@@ -11,45 +11,13 @@ def NearestNeighbor (x,k,l,binary=False):
 	
 	inf = 1e20;
 	n = len(x)
-	sort = [[]]*l
-	for i in range(0,len): 
+	res = range(0,l) 
+	stockDist = []*n
+	for i in range(0,n): 
 		distance = [inf]*n
-		for j in range(0,len):
+		for j in range(0,n):
 			if i != j and !binary :
 				distance[j] = np.linalg.norm(x[i]-x[j])
-				
-
-				
-				
-	
-def KLower (k,x) : 
-	"""
-	Return the indice of the k lower values in x
-	k : the number of indices to obtain
-	x : values to order
-	"""
-	
-	res = False
-	taille = len(x)
-	
-	if k > taille :
-		print("You cannot take the k lower values in x if k is higher than the number of values in x")
-	else :
-		indRes = range(0,k)
-		valRes = x[0:k-1]
-		for i in range(k,taille):
-		big = bigger(valRes)
-			ind = indRes[big]
-			if x(i) < x(ind]):
-				indRes[big]=i
-				valRes[ind]=x(i)
-	return indRes
-	
-def bigger (x):
-	"""
-	The list of values whereyou want to find the bigger value
-	"""
-	maxi = max(x)
-	ind = x.find(maxi)
-	return ind
+		low = distance.sort()[0:k-1]
+		stockDist[i] = np.mean(low)
 	
