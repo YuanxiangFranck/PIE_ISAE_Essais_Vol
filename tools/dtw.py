90b7ce85cd2f3fe659f3a4fe25711e8352c63793
Florent Forest <florent.forest9@gmail.com>
2016-11-22 19:30:56 +0100

    Update dtw.py + implement DTW_align_plot

--- tools/dtw.py
+++ tools/dtw.py
@@ -10,16 +10,21 @@ Dynamic Time Warping
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 def DTW_dist(S,T):
     "Compute DTW distance between two timeseries"
     return DTW(S,T)[-1,-1] # return last element of DTW matrix
 
-def DTW_align(S,T):
-    "Compute DTW alignment of two timeseries" 
+def DTW_align(S,T,dtw=None):
+    """
+    Compute DTW alignment of two timeseries
+    optional argument dtw : DTW matrix of the two signals 
+    """
     n = len(S)
     m = len(T)
-    dtw = DTW(S,T)
+    if dtw == None:
+        dtw = DTW(S,T)
     i,j = 0,0
     align = [[i,j]]
     while(i < n-1 or j < m-1):
@@ -33,23 +38,53 @@ def DTW_align(S,T):
             i = i+1
         else:
             # choose the shorter path in matrix
-            idx = np.argmin([dtw[i+1,j],dtw[i,j+1],dtw[i+1,j+1]])
+            idx = np.argmin([dtw[i+1,j+1],dtw[i+1,j],dtw[i,j+1]])
             if idx == 0:
-                # go forward in S 
-                i = i+1
-            elif idx == 1:
-                # go forward in T
-                j = min(j+1,m-1)
-            else:
                 # go forward in both S and T
                 i = i+1
+                j = j+1
+            elif idx == 1:
+                # go forward in S 
+                i = i+1
+            else:
+                # go forward in T
                 j = j+1
         align.append([i,j])
     return(np.array(align))
             
+def DTW_align_plot(S,T,dtw=None,sig_names=['Sig1','Sig2'],style='k--'):
+    """
+    Returns matplotlib plot of the DTW alignment of two timeseries
+    optional argument dtw : DTW matrix of the two signals 
+    """
+    align = DTW_align(S,T,dtw)
+    
+    fig = plt.figure()
+    sig1, = plt.plot(S,'r')
+    sig2, = plt.plot(T,'b')
+    plt.legend((sig1,sig2),(sig_names[0],sig_names[1]))
+    plt.xlabel('Time')
+    for k in range(len(align)):
+        i = align[k,0]
+        j = align[k,1]
+        plt.plot([i,j],[S[i],T[j]],style)
+    
+    plt.title('DTW alignment of {} and {}'.format(sig_names[0],sig_names[1]))
+    return fig
 
 def DTW(S,T):
-    "Compute DTW matrix of two timeseries"
+    """
+    Compute DTW matrix of two timeseries
+    
+    Caution : n*m complexity !
+    
+    Benchmark
+      n  | time on Core i3
+    --------
+     100 |    566 ms
+     200 |  2 260 ms
+    1000 | 57 500 ms
+    """
     n = len(S)
     m = len(T)
     dtw = np.zeros((n,m))
