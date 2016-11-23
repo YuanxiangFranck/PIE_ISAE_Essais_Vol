2d02e8007ea0d4e22e223d9aff94c4b1c77bfc9d
Quentin <qjacob.jacob@gmail.com>
2016-11-23 09:53:32 +0100

    add filter on Vza

--- dataProcessing/segmenter.py
+++ dataProcessing/segmenter.py
@@ -6,7 +6,7 @@ Created on Sat Nov  12 13:25:24 2016
 
 Outil pour segmenter les vols
 """
-
+#TO DO : poids associé à chaque segment, regarder la couverture, filtre sur la Vza (1 minute), regarder aussi si on est sur HP/IHP
 #%%
 
 import sys,os
@@ -30,22 +30,20 @@ def cut(time_list):
     """
     if not time_list:
         return []
-    else :
-        jumps = []
-        for i in range(len(time_list)-1):
-            if time_list[i + 1] != time_list[i] + 1:
-                jumps.append((time_list[i],time_list[i+1]))
-        if not jumps:
-            return [(time_list[0],time_list[-1])] # if time values are continuous, start time = first time, end time = last time
-        elif len(jumps)==1:
-            return [(time_list[0],jumps[0][0]),(jumps[0][1],time_list[-1])] # If there is one single jump, one segment before the jump, one                                                                         segment after
-        else :
-            dates = []
-            dates.append((time_list[0],jumps[0][0])) # First segment
-            for i in range(len(jumps)-1):
-                dates.append((jumps[i][1],jumps[i+1][0])) # Intermediate segments
-            dates.append((jumps[-1][1],time_list[-1])) # Last segment
-            return dates
+    jumps = []
+    for i in range(len(time_list)-1):
+        if time_list[i + 1] != time_list[i] + 1:
+            jumps.append((time_list[i],time_list[i+1]))
+    if not jumps:
+        return [(time_list[0],time_list[-1])] # if time values are continuous, start time = first time, end time = last time
+    elif len(jumps)==1:
+        return [(time_list[0],jumps[0][0]),(jumps[0][1],time_list[-1])] # If there is one single jump, one segment before the jump, one                                                                        segment after
+    dates = []
+    dates.append((time_list[0],jumps[0][0])) # First segment
+    for i in range(len(jumps)-1):
+        dates.append((jumps[i][1],jumps[i+1][0])) # Intermediate segments
+    dates.append((jumps[-1][1],time_list[-1])) # Last segment
+    return dates
 
 def segment(data, otg=True, take_off=True, climb=True, hold=True, cruise=True, descent=True):
     """
@@ -85,7 +83,7 @@ def segment(data, otg=True, take_off=True, climb=True, hold=True, cruise=True, d
     wow_signal = data[wow].iloc[:,0]
     altitude_signal = data[altitude].iloc[:,0]
     cas_signal = data[calib_air_speed].iloc[:,0]
-    alt_rate_signal = data[altitude_rate].iloc[:,0]
+    alt_rate_signal = data[altitude_rate].iloc[:,0].rolling(center = False, window = 60).mean()
     on_the_ground = (wow_signal==1) & (cas_signal < 80) & (altitude_signal < 15000)
     intervals = dict()
     if otg:
