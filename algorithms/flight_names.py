e389e35f7f94b0f287611aff8ddc975db2e21648
Florent Forest <florent.forest9@gmail.com>
2017-01-05 19:13:33 +0100

    add file containing flight names

---
+++ algorithms/flight_names.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 19:11:55 2017
+
+@author: Florent
+
+Flight Names
+"""
+
+flight_names = \
+['E190-E2_20001_0083_29106_52495_request.txt',
+ 'E190-E2_20001_0085_29472_53398_request.txt',
+ 'E190-E2_20001_0085_29472_53399_request.txt',
+ 'E190-E2_20001_0086_30023_54638_request.txt',
+ 'E190-E2_20001_0088_29574_53580_request.txt',
+ 'E190-E2_20001_0089_29794_54071_request.txt',
+ 'E190-E2_20001_0089_29800_54082_request.txt']
+ 
