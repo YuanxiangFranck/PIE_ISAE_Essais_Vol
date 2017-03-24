fc3df371a06bb9646b4f34a843745f6f7f43068a
Franck WANG <franckwang123@gmail.com>
2017-03-25 00:04:32 +0100

    remove plotly logo + update doc

--- dataProcessing/html_page/index.js
+++ dataProcessing/html_page/index.js
@@ -8,10 +8,22 @@ Plotly.register([
     // require('plotly.js/lib/box')
 ]);
 
-// Global variable from html:
+// Handle window resiwe
+window.addEventListener("resize", e=>{
+    for (let node of document.querySelectorAll("div.plot-container.plotly")){
+        Plotly.Plots.resize(node.parentNode);
+    }
+});
+/*
+Global variable from html:
+* stats
+* phases
+* port_plot_data_1
+* port_plot_data_2
+* ports_side_data
+* ports_seg_data
 
-// stats: data for phase % to plot pie chart
-// phases: flight phases
+*/
 
 function plot_phases(){
     let data = [];
@@ -32,7 +44,7 @@ function plot_phases(){
                   xaxis: {title: "Time (s)"},
                   yaxis: {title: "Phase", range:[0,8]}
                  };
-    Plotly.newPlot('plot_phases', data, layout);
+    Plotly.newPlot('plot_phases', data, layout, {displaylogo: false});
 }
 
 function plot_ports(){
@@ -44,9 +56,9 @@ function plot_ports(){
                   xaxis: {title: "Time (s)"},
                   yaxis: {title: "Ports", range:[0,8]}
                  };
-    Plotly.newPlot("plot_ports_side_1", port_plot_data_1, layout);
+    Plotly.newPlot("plot_ports_side_1", port_plot_data_1, layout, {displaylogo: false});
     layout.title = "Flight ports usage side 2";
-    Plotly.newPlot("plot_ports_side_2", port_plot_data_2, layout);
+    Plotly.newPlot("plot_ports_side_2", port_plot_data_2, layout, {displaylogo: false});
 }
 
 function plot_ratio_pie_chart(){
@@ -70,7 +82,7 @@ function plot_ratio_pie_chart(){
         height: 400,
         width: 500
     };
-    Plotly.newPlot('phases_stats_pie', data, layout);
+    Plotly.newPlot('phases_stats_pie', data, layout, {displaylogo: false});
 
 }
 
@@ -80,7 +92,7 @@ function plot_port_usage_per_phases(){
     layout = {height: 400};
     layout.annotations = [{font: {size: 20}, showarrow: false, text:"Side: 1", x: 0.2, y:1},
                           {font: {size: 20}, showarrow: false, text:"Side: 2", x: 0.8, y:1}];
-    Plotly.newPlot('port_usage_side', ports_side_data, layout);
+    Plotly.newPlot('port_usage_side', ports_side_data, layout, {displaylogo: false});
     layout.height = 700;
     layout.annotations = [];
     for (let port_data of ports_seg_data){
@@ -89,7 +101,7 @@ function plot_port_usage_per_phases(){
         layout.annotations.push({font: {size: 20}, showarrow: false,
                                  text: port_data.name, x: x, y: y});
     }
-    Plotly.newPlot('port_usage_phases', ports_seg_data, layout);
+    Plotly.newPlot('port_usage_phases', ports_seg_data, layout, {displaylogo: false});
 }
 
 plot_ratio_pie_chart();
